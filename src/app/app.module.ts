b143a16ad48bcba4cc40032057e32ca5dca65b94
Mykola Samila <mykola.samila@drillinginfo.com>
2019-11-26 20:02:51 +0200

    add icon and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
@@ -41,7 +42,8 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
     MatInputModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms'
+import { FormControl, Validators } from '@angular/forms';
+import { TodoItem } from './todo-item';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,17 +8,35 @@ import { FormControl, Validators } from '@angular/forms'
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  task = new FormControl('', [Validators.required]);
+  private todoListItems: TodoItem[];
+  private task: FormControl;
+
+  constructor() { }
+
+  ngOnInit() {
+  this.task = new FormControl('', [Validators.required]);
+
+    this.todoListItems = [
+      {
+        title: 'Feed Dog',
+        // description: 'give dog some meat',
+        done: false,
+      },
+
+      {
+        title: 'Buy some foods',
+        // description: 'buy oil, meat for dog, some flour, onion, salt and dumpings',
+        done: false
+      }
+    ];
+    console.log(this.todoListItems);
+  }
+
 
   getErrorMessage() {
     return this.task.hasError('required') ? 'You must enter a value' :
       this.task.hasError('email') ? 'Not a valid email' :
         '';
-  }
-
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
